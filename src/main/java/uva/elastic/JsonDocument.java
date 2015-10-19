fa6bb72f7c2433ffd9d70255f7526eaaa02d73a8
Julian Thijssen <Nimthora@gmail.com>
2015-10-19 23:45:36 +0200

    Initial bulk of project

---
+++ src/main/java/uva/elastic/JsonDocument.java
@@ -0,0 +1,9 @@
+package uva.elastic;
+
+public class JsonDocument {
+	public String id = "";
+	public String title = "";
+	public String text = "";
+	public String date = "";
+	public String subject = "";
+}
