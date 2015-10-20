b60118755a5c963c23a061ba789f1cb123937003
Julian Thijssen <Nimthora@gmail.com>
2015-10-20 20:39:23 +0200

    Removed old files

--- src/main/java/uva/elastic/JsonDocument.java
+++
@@ -1,9 +0,0 @@
-package uva.elastic;
-
-public class JsonDocument {
-	public String id = "";
-	public String title = "";
-	public String text = "";
-	public String date = "";
-	public String subject = "";
-}
