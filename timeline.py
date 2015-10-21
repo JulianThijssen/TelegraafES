648c70f52cd777421b99625e645d11fd606ef6e9
root <julian.thijssen@student.uva.nl>
2015-10-21 11:02:37 -0400

    Fixed number of ticks on y-axis of timeline

--- timeline.py
+++ timeline.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.ticker as ticker
 
-x = np.arange(1960, 1999, 1)
+x = np.arange(1960, 1995, 1)
 years = dict.fromkeys(x, 0);
 
 with open("years") as f:
@@ -16,11 +16,12 @@ for line in lines:
     years[year] += 1
 
 plt.figure(1, figsize=(8, 2))
-plt.xlim(1960, 1999)
+plt.xlim(1960, 1995)
 ax = plt.axes()
-ax.xaxis.set_tick_params(length=5)
+ax.xaxis.set_tick_params(length=1)
+ax.yaxis.set_major_locator(ticker.MaxNLocator(4))
 ax.get_xaxis().set_minor_locator(ticker.MultipleLocator(1))
-ax.get_yaxis().set_major_locator(ticker.MultipleLocator(1))
+#ax.get_yaxis().set_major_locator(ticker.MultipleLocator(1))
 plt.stem(years.keys(), years.values())
 
 plt.savefig("timeline")
